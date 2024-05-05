ee0a55de8cc238c47e284fa5d1b128988dc1e873
Arda Avcı <wonderluxct@gmail.com>
2024-05-05 16:57:12 +0300

    Create harf.java

---
+++ src/harf.java
@@ -0,0 +1,24 @@
+public class Harf {
+    private String deger;
+    private boolean tahmin;
+
+    public Harf(String deger) {
+        this.deger = deger;
+    }
+
+    public String getDeger() {
+        return deger;
+    }
+
+    public void setDeger(String deger) {
+        this.deger = deger;
+    }
+
+    public boolean isTahmin() {
+        return tahmin;
+    }
+
+    public void setTahmin(boolean tahmin) {
+        this.tahmin = tahmin;
+    }
+}
